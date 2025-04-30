dbabe2b80edc6d03ca35fdebdeba9e296a532cb7
chughishnik <chughishnik007mail.ru@gmail.com>
2025-04-30 21:15:38 +0300

    Update ping-pong.py
    v2.0

--- ping-pong.py
+++ ping-pong.py
@@ -7,10 +7,11 @@ clock = time.Clock()
 win = display.set_mode((700,500))
 display.set_caption('ping-pong')
 
+font.init()
+font = font.SysFont('Arial',40)
 
-win.fill((119, 136, 153))
-
-
+won_1 = font.render('победил игрок № 1',1,(0,255,0))
+won_2 = font.render('победил игрок № 2',1,(0,255,0))
 class Gamesprite(sprite.Sprite):
 
     def __init__(self,image_1,x,y,speed,width,hight):
@@ -27,27 +28,67 @@ class Gamesprite(sprite.Sprite):
 
 
 class Player(Gamesprite):
-    def update(self):
+
+    def update1(self):
         keys = key.get_pressed()
-        curent_speed = self.speed*2 if keys[K_LSHIFT] else self.speed
 
-        if keys[K_d] and self.rect.x < 620:
-            self.rect.x += curent_speed
+        if keys[K_s] and self.rect.y < 350:
+            self.rect.y += self.speed
 
-        if keys[K_a] and self.rect.x > 5:
-            self.rect.x -= curent_speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
 
-    def shoot(self):
-        bullet = Bulet('bullet.png',player.rect.centerx,player.rect.top,5,10,10)
-        bullets.add(bullet)
-        fire.play()
+    def update2(self):
+        keys = key.get_pressed()
+
+        if keys[K_DOWN] and self.rect.y < 350:
+            self.rect.y += self.speed
+
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
 
 
+
+ball = Gamesprite('tenis_ball.png',350,200,3,50,50)
+
+player_1 = Player('racket.png',5,5,5,50,150)
+player_2 = Player('racket.png',645,5,5,50,150)
+
+
+speed_x = 3
+speed_y = -3
 
 game = True
+finish = False
 while game:
-    
+        
+    if finish != True:
+        win.fill((119, 136, 153))
 
+        ball.place()
+
+        player_1.place()
+        player_1.update1()
+
+        player_2.place()
+        player_2.update2()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y <5 or ball.rect.y >450:
+            speed_y *= -1
+
+        if sprite.collide_rect(player_1,ball) or sprite.collide_rect(player_2,ball):
+            speed_x *= -1
+
+        if ball.rect.x >= 700:
+            finish = True
+            win.blit(won_1,(220,200))
+
+        if ball.rect.x <= 0:
+            finish = True
+            win.blit(won_2,(220,200))
 
     for i in event.get():
         if i.type == QUIT:
