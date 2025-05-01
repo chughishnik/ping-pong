d3f29460e48523ba14afbfa959c65cef3127430e
chughishnik <chughishnik007mail.ru@gmail.com>
2025-05-01 15:53:09 +0300

    Update ping-pong.py
    v3.0

--- ping-pong.py
+++ ping-pong.py
@@ -12,6 +12,7 @@ font = font.SysFont('Arial',40)
 
 won_1 = font.render('победил игрок № 1',1,(0,255,0))
 won_2 = font.render('победил игрок № 2',1,(0,255,0))
+rst = font.render('нажмите r чтобы взять реванш',1,(255,0,0))
 class Gamesprite(sprite.Sprite):
 
     def __init__(self,image_1,x,y,speed,width,hight):
@@ -54,16 +55,26 @@ ball = Gamesprite('tenis_ball.png',350,200,3,50,50)
 player_1 = Player('racket.png',5,5,5,50,150)
 player_2 = Player('racket.png',645,5,5,50,150)
 
+# finish = True
+# win.blit(won_1,(220,200))
 
 speed_x = 3
 speed_y = -3
 
 game = True
 finish = False
+score_1 = 0
+score_2 = 0
 while game:
         
     if finish != True:
         win.fill((119, 136, 153))
+
+        text_score_1 = font.render('очки: '+str(score_1),1,(255, 215, 0))
+        win.blit(text_score_1,(10,0))
+
+        text_score_2 = font.render('очки: '+str(score_2),1,(255, 215, 0))
+        win.blit(text_score_2,(570,0))
 
         ball.place()
 
@@ -83,16 +94,41 @@ while game:
             speed_x *= -1
 
         if ball.rect.x >= 700:
-            finish = True
-            win.blit(won_1,(220,200))
+            score_1 += 1
+            speed_x *= -1
+            ball.rect.x = 350
+            ball.rect.y = 200
 
         if ball.rect.x <= 0:
+            score_2 += 1
+            speed_x *= -1
+            ball.rect.x = 350
+            ball.rect.y = 200
+
+        if score_1 >= 5:
+            finish = True
+            win.blit(won_1,(220,200))
+            win.blit(rst,(150,300))
+
+        if score_2 >= 5:
             finish = True
             win.blit(won_2,(220,200))
+            win.blit(rst,(120,300))
 
     for i in event.get():
         if i.type == QUIT:
             game = False
+
+        if i.type == KEYDOWN:           
+            if i.key == K_r and finish == True:
+                score_1= 0
+                score_2 = 0
+                finish = False
+
+                ball.rect.x = 350
+                ball.rect.y = 200
+
+
 
     display.update()
     clock.tick(fps)
